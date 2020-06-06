f4f460387bdcf362b8d2ea7812f453bb01b15088
EmmadRaheem <EmmadRaheem@users.noreply.github.com>
2020-06-06 08:13:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,16 @@ function App() {
         </p>
         <br />
         <p>
-        One of the most familiar diagnostic methods is the use of venous blood as a test in a medical laboratory. This needs an intrusive cannulation approach which requires proper vein selection. The use of a vein finder will help the phlebotomist identify the vein quickly, avoid possible pre-analytical mistakes in the specimen collection and reduce patients' anxiety and pain. Cannulation challenges doctors due to the various factors that can be lessened by the use of a vein detector. In addition, changes in methodology, patient numbers, demographic type of patients and equipment used in evaluating prototypes developed have been reported. Some studies in the actual human prototype test were limited. The production of a low-cost infrared vein detector (IR) needs to be improved. commercialization. Eventually, it was reported that there were minimal publications on phlebotomy efficiency evaluations using vein detectors.
+        One of the most familiar diagnostic methods is the use of venous blood as a test in a medical laboratory. 
+        This needs an intrusive cannulation approach which requires proper vein selection. The use of a vein 
+        finder will help the phlebotomist identify the vein quickly, avoid possible pre-analytical mistakes in 
+        the specimen collection and reduce patients' anxiety and pain. Cannulation challenges doctors due to 
+        the various factors that can be lessened by the use of a vein detector. In addition, changes in 
+        methodology, patient numbers, demographic type of patients and equipment used in evaluating prototypes
+         developed have been reported. Some studies in the actual human prototype test were limited. The 
+         production of a low-cost infrared vein detector (IR) needs to be improved. commercialization. 
+         Eventually, it was reported that there were minimal publications on phlebotomy efficiency 
+         evaluations using vein detectors.
         </p>
         <a
           className="App-link"
